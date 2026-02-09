d5e0645a47401456b9b881c94f274830ca618e6a
tijn <tijn@tijn.dev>
2026-02-09 23:27:56 +0100

    feat: allow passing props to Toaster via url params

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { createRoot } from "react-dom/client";
 import { toast, Toaster } from "sonner";
 
+const params = (() => {
+  try {
+    return Object.fromEntries(
+      new URL(document.currentScript?.src).searchParams.entries(),
+    );
+  } catch (e) {
+    return {};
+  }
+})();
+
 const container = document.createElement("div");
 document.body.appendChild(container);
 
 const root = createRoot(container);
-root.render(<Toaster />);
+root.render(<Toaster {...params} />);
 
 window.toast = toast;
